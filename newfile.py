b14d9d64408ae89288f535d48f29b38c6ba0f2c9
lyallsathish <73095383+lyallsathish@users.noreply.github.com>
2021-03-30 08:04:18 +0530

    new changes are made

--- newfile.py
+++ newfile.py
@@ -3,6 +3,9 @@ class Student:
   def __init__(self,name,age):
     self.name=name
     self.age=age
- s1 = student("lyall",20)
+class MyClass(Student):
+  pass
+s1 = student("lyall",20)
+s2 = MyClass("aksh",19)
 print(s1.name)
 print(s1.age)
